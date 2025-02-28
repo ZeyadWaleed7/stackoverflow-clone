4cc3bfb28d4fd983dc10b53a91a0c732e9c54edb
zeyad waleed <s-zeyad.nabil@zewailcity.edu.eg>
2025-02-28 19:29:52 +0200

    Adding tweet component

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -2,13 +2,15 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login/Login";
 import Home from "./pages/Home";
 import Signup from "./pages/Signup/Signup";
+import Tweet from "./components/Tweet/Tweet";
 
 const App = () => {
   return (
     <Router>
-      <Signup />
-       {/* <Home />    
-        <Routes>
+      <Tweet />
+        {/* <Signup />
+        <Home /> 
+         <Routes>
           <Route path="/" element={<Login />} />
         </Routes> */}
     </Router>
