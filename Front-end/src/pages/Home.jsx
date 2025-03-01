e81b978cf3f417f02a7ee3920798f35dc0aeaa7a
zeyad waleed <s-zeyad.nabil@zewailcity.edu.eg>
2025-03-01 17:12:08 +0200

    Adding Feed

--- Front-end/src/pages/Home.jsx
+++ Front-end/src/pages/Home.jsx
@@ -1,14 +1,16 @@
 import Navbar from "../components/Navbar/Navbar";
 import Sidebar from "../components/Sidebar/Sidebar";
 import Rightbar from "../components/Rightbar/Rightbar";
+import Feed from "../pages/Feed/Feed";
 
 const Home = () => {
   return (
     <div className="layout">
-      <Navbar />
+      {/* <Navbar /> */}
       <div className="content">
         <Sidebar />
         <main className="main-content">
+          <Feed />
         </main>
         <Rightbar />
       </div>
