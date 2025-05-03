f16b0b6e620bf8ec972df695f4cfccd7418526c3
MohyEldeen <mohym3295@gmail.com>
2025-05-03 07:26:46 +0300

    edited rebuilt QA & redis docker containers

--- backend/q&a/test1.js
+++ backend/q&a/test1.js
@@ -23,6 +23,6 @@ app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ error: 'Something went wrong!' });
 });
-console.log("contaienr test");
+console.log("i hope you end up dead");
 app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
 
